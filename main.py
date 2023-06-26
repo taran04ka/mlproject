de01259a42b6533fe5d8fc3245fa63c9485b7337
Taras Zhyhulin <zhyhulin@student.agh.edu.pl>
2023-06-26 22:57:10 +0200

    added pruning and oop

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-# main.py
-from data.data_loader import load_data
-from models.model_builder import build_model, train_model
-from utils.evaluation import evaluate_model, plot_training_history
+from data.data_loader import DataLoader
+from models.model_builder import ModelBuilder
+from utils.evaluation import Evaluator
+import tensorflow as tf
+import tensorflow_model_optimization as tfmot
 
 def main():
-    X_train, X_val, y_train, y_val, test_images, test_labels = load_data()
-    model = build_model()
-    history = train_model(model, X_train, y_train, X_val, y_val, epochs=20, batch_size=128)
-    plot_training_history(history)
-    evaluate_model(model, test_images, test_labels)
+    if tf.test.is_gpu_available():
+        print("GPU available")
+    else:
+        print("GPU not available")
+    tf.config.list_physical_devices('GPU')
+
+    data_loader = DataLoader()
+    X_train, X_val, y_train, y_val, test_images, test_labels = data_loader.load_data()
+    model_builder = ModelBuilder()
+
+    model = model_builder.build_model()
+    history = model_builder.train_model(model, X_train, y_train, X_val, y_val, epochs=3, batch_size=128)
+    evaluator = Evaluator()
+    evaluator.plot_training_history(history)
+    evaluator.evaluate_model(model, test_images, test_labels)
     model.save('fashion_mnist_model.h5')
+
+    pruned_model = model_builder.build_pruned_model()
+    pruned_model.summary()
+    pruned_model.fit(X_train, y_train, batch_size=128, epochs=3, validation_data=(X_val, y_val),
+                     callbacks=[tfmot.sparsity.keras.UpdatePruningStep()])
+    pruned_model.save('pruned_fashion_mnist_model.h5')
+
+    pruned_history = model_builder.train_model(pruned_model, X_train, y_train, X_val, y_val, epochs=3, batch_size=128)
+    evaluator = Evaluator()
+    evaluator.plot_training_history(pruned_history)
+    evaluator.evaluate_model(pruned_model, test_images, test_labels)
+    pruned_model.save('fine_tuned_fashion_mnist_model.h5')
 
 if __name__ == '__main__':
     main()
--- models/model_builder.py
+++ models/model_builder.py
@@ -1,20 +1,41 @@
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 from tensorflow.keras.optimizers import Adam
+import tensorflow_model_optimization as tfmot
+from tensorflow_model_optimization.sparsity.keras import UpdatePruningStep
 
-def build_model(optimizer='adam', activation='relu', dropout_rate=0.0):
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(128, activation=activation))
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(10, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    return model
+class ModelBuilder:
+    def build_model(self, optimizer='adam', activation='relu', dropout_rate=0.0):
+        with tf.device('/GPU:0'):
+            model = Sequential()
+            model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Flatten())
+            model.add(Dense(128, activation=activation))
+            model.add(Dropout(dropout_rate))
+            model.add(Dense(10, activation='softmax'))
+            model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        return model
 
-def train_model(model, X_train, y_train, X_val, y_val, epochs=10, batch_size=32):
-    history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, y_val))
-    return history
+    def build_pruned_model(self, optimizer='adam', activation='relu', dropout_rate=0.0):
+        model = self.build_model(optimizer, activation, dropout_rate)
+        pruning_params = {
+            'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50,
+                                                                     final_sparsity=0.90,
+                                                                     begin_step=0,
+                                                                     end_step=2000)
+        }
+        model_pruned = tfmot.sparsity.keras.prune_low_magnitude(model, **pruning_params)
+        model_pruned.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        return model_pruned
+
+    def train_model(self, model, X_train, y_train, X_val, y_val, epochs=3, batch_size=32):
+        callbacks = [
+            UpdatePruningStep(),
+        ]
+        history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
+                            validation_data=(X_val, y_val), callbacks=callbacks)
+        return history
